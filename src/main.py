1575f6c28c24d4873dadaafb99f56335dd023d9b
Nikita <stlrvnk@gmail.com>
2024-02-22 11:22:41 +0200

    endpoints templates

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
-# индекс для теста
-# /cookies (post - get request and send to another url)
-#           (options - return {"success": True} )
-from fastapi import FastAPI, Query, status
+from fastapi import FastAPI
+import uvicorn
 
 
 app = FastAPI()
@@ -10,3 +8,22 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"success": True}
+
+
+@app.options("/")
+async def index_options():
+    ...
+
+
+@app.post("/cookies")
+async def cookies():
+    ...
+
+
+@app.options("/cookies")
+async def cookies_options():
+    ...
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
