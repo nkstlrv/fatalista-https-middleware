671bc0f12af3fced70196711ce8e2778965cf905
Nikita <stlrvnk@gmail.com>
2024-02-22 12:56:45 +0200

    Logging improved; prod endpoint connected

--- src/main.py
+++ src/main.py
@@ -1,11 +1,44 @@
 from urllib import response
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from logging_config import logger
 import requests
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:5000",
+
+    "https://aws-https-midleware.onrender.com/aws",
+    "https://aws-https-midleware.onrender.com",
+
+    "https://52.41.36.82:5000",
+    "https://54.191.253.12:5000",
+    "https://44.226.122.3:5000",
+
+    "https://52.41.36.82:5000/aws",
+    "https://54.191.253.12:5000/aws",
+    "https://44.226.122.3:5000/aws",
+
+    # dragontail
+    "https://dragontail.choiceqr.com"
+
+    # fatalis
+    "https://fatalistbar.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -20,12 +53,29 @@ async def index_options():
 
 
 @app.post("/cookies")
-async def cookies(payload: dict):
-    response = requests.post("https://postman-echo.com/post", json=payload)
+async def cookies(request: Request):
+
+    try:
+        payload = await request.json()
+    except Exception as ex:
+        logger.error(f"!!! ERROR OCCURRED - {ex}")
+        raise HTTPException(status_code=422,
+                            detail="Bad Payload")
+    
+
+    logger.info("SENDING DATA TO AWS")
+    response = requests.post("http://52.23.187.142:5000/post_cookies_fatalist", json=payload)
+
+    aws_status_code = response.status_code
+    aws_data = response.json()
+
+    logger.info(f"AWS STATUS CODE - {aws_status_code}; AWS RESPONSE - {aws_data}")
+
     if response.status_code == 200:
-        logger.info(f"Received payload data: {payload}")
-        return response.json()
+        logger.info(f"SUCCESSFULLY SENT DATA TO AWS")
+        return aws_data
     else:
+        logger.warning("! FAILED SENDING DATA TO AWS")
         raise HTTPException(status_code=response.status_code,
                             detail="Request failed")
 
@@ -40,4 +90,5 @@ async def cookies_options():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app=app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app=app, host="0.0.0.0", port=8000)
+    uvicorn.run(app=app, host="127.0.0.1", port=8000)
